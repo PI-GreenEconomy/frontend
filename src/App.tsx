8e752417b360d326a1c0dc43bb6927752e6d7275
Ingrid Alves <alves96.ingrid@gmail.com>
2024-05-21 11:43:28 -0300

    feat: Adiciodo Estrutura de Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/header/Header";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import { Footer } from "./components/footer/Footer";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
+         <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
   )
 }
