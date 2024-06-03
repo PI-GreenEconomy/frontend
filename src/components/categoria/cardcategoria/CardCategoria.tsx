743541dd77e0db9e1119664eee2d42e7778c3909
StephanieSST <stephaniesteuernagel@gmail.com>
2024-06-03 11:30:42 -0300

    feat: Criação de CardCategoria

--- src/components/categoria/cardcategoria/CardCategoria.tsx
+++ src/components/categoria/cardcategoria/CardCategoria.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import Categoria from "../../../models/Categoria";
 
-export const CardCategoria = () => {
-  return <div>CardCategoria</div>;
-};
+interface CardCategoriaProps {
+  categoria: Categoria;
+}
+
+function CardCategoria({ categoria }: CardCategoriaProps) {
+  return (
+    <div className="flex">
+      <header className="font-bold">Categoria</header>
+      <p className="h-full">{categoria.tipo}</p>
+
+      <div className="flex">
+        <Link
+          to={`/editarcategoria/${categoria.id}`}
+          className="flex w-full items-center justify-center"
+        >
+          <button>Editar</button>
+        </Link>
+
+        <Link
+          to={`/deletarcategoria/${categoria.id}`}
+          className="flex w-full items-center justify-center"
+        >
+          <button>Deletar</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default CardCategoria;
