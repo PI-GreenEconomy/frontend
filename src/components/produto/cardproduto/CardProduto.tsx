d5acf5ec17c8e06a5e778e72b715d73644ee23c5
MariaBenitesJones <mariaeduardabmrj@gmail.com>
2024-06-04 11:09:12 -0300

    feat: CardProduto implementado

--- src/components/produto/cardproduto/CardProduto.tsx
+++ src/components/produto/cardproduto/CardProduto.tsx
@@ -1,3 +1,42 @@
-export const CardProduto = () => {
-  return <div>CardProduto</div>;
-};
+import { Link } from "react-router-dom";
+import Produto from "../../../models/Produto";
+
+interface CardProdutoProps {
+  produto: Produto;
+}
+
+export function CardProduto({ produto }: CardProdutoProps) {
+  return (
+    <div className="flex flex-col justify-between overflow-hidden rounded border border-slate-900">
+      <div>
+        <div className="flex w-full items-center gap-4 bg-indigo-400 px-4 py-2">
+          <img src={produto.foto} className="h-12 rounded-full" alt="" />
+          <h3 className="text-center text-lg font-bold uppercase ">
+            {produto.nome}
+          </h3>
+        </div>
+        <div className="p-4 ">
+          <h4 className="text-lg font-semibold uppercase">
+            R$ {produto.basePreco}
+          </h4>
+          <p>{produto.descricao}</p>
+          <p>Tema: {produto.categoria?.tipo}</p>
+        </div>
+      </div>
+      <div className="flex">
+        <Link
+          to={`/editarprodutoagem/${produto.id}`}
+          className="flex w-full items-center justify-center bg-indigo-400 py-2 text-white hover:bg-indigo-800"
+        >
+          <button>Editar</button>
+        </Link>
+        <Link
+          to={`/deletarprodutoagem/${produto.id}`}
+          className="flex w-full items-center justify-center bg-red-400 text-white hover:bg-red-700"
+        >
+          <button>Deletar</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
