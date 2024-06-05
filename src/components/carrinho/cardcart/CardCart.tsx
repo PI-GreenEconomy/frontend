9e1e0892c94dd48a20d549721404e4b5f3aed295
joyceravie <joyce.rvs@hotmail.com>
2024-06-05 20:41:18 -0300

    refactor: adiciona estilização carrinho de compras

--- src/components/carrinho/cardcart/CardCart.tsx
+++ src/components/carrinho/cardcart/CardCart.tsx
@@ -10,29 +10,31 @@ function CardCart({ item }: CardProdutosProps) {
   const { removerProduto } = useContext(CartContext);
 
   return (
-    <div className="flex flex-col justify-between overflow-hidden rounded-lg bg-white">
+    <div className="align-center flex flex-col justify-between overflow-hidden rounded-lg bg-white shadow-md">
       <div className="py-4">
         <img
           src={item.foto}
-          className="max-w-75 mx-auto mt-1 h-40"
+          className="max-w-75 mx-auto mt-1 h-40 rounded-lg"
           alt={item.nome}
         />
 
         <div className="p-4">
-          <p className="text-center text-sm uppercase">{item.nome}</p>
+          <p className="text-center text-sm font-bold uppercase">{item.nome}</p>
           <h3 className="text-center text-xl font-bold uppercase">
             {Intl.NumberFormat("pt-BR", {
               style: "currency",
               currency: "BRL",
             }).format(item.basePreco)}
           </h3>
-          <p className="text-center text-sm italic">Categoria: Tipo </p>
+          <p className="text-center text-sm font-bold text-primary">
+            Categoria: Tipo{" "}
+          </p>
         </div>
       </div>
       <div className="flex flex-wrap">
         <button
           className="flex w-full items-center justify-center 
-                                   bg-red-500 py-2 text-slate-100 hover:bg-red-700"
+                                   bg-terra py-2 font-bold uppercase text-slate-100 hover:bg-[#ca8d50]"
           onClick={() => removerProduto(item.id)}
         >
           Remover
--- src/components/carrinho/cart/Cart.tsx
+++ src/components/carrinho/cart/Cart.tsx
@@ -8,13 +8,12 @@ function Cart() {
   return (
     <div
       className="
-                flex 
-                flex-col 
-                justify-center
-                bg-gray-200
+                align-center 
+                my-8 
+                flex flex-col justify-center
                 "
     >
-      <h1 className="my-4 text-center text-4xl">Carrinho de Compras</h1>
+      <h1 className="my-8 text-center text-4xl">Carrinho de Compras</h1>
       <h2 className="my-4 text-center text-2xl">
         {items.length === 0 ? "O Carrinho está vazio!" : ""}
       </h2>
@@ -28,8 +27,8 @@ function Cart() {
       </div>
 
       <button
-        className="mx-auto flex w-1/4 
-          justify-center rounded bg-slate-400 py-2 text-slate-100 hover:bg-slate-800"
+        className="mx-auto my-10 flex 
+          w-1/4 justify-center rounded bg-primary py-2 font-bold uppercase text-slate-100 hover:bg-[#357a62]"
         type="submit"
         disabled={items.length === 0 ? true : false}
         onClick={limparCart}
