343d42c67bbf19a1f6b25405534806846ab1a620
senhorkaioh <caiopsantos.dev@gmail.com>
2024-06-06 13:39:08 -0300

    feat: adição do do quemsomos missão - Lucas

---
+++ src/pages/quem-somos/QuemSomosBody.tsx
@@ -0,0 +1,128 @@
+export default function QuemSomosBody() {
+  return (
+    // principal
+    <div className="relative text-white">
+      {/* container1 */}
+      <div>
+        {/* TESTE INICIO */}
+        <div className="absolute left-0 top-20 z-20 mx-5 my-20 rounded-lg backdrop-blur-sm  md:p-20 ">
+          <h1 className="my-8 font-poppins font-bold  md:text-6xl">
+            Compre com Consciência,
+            <br />
+            Viva com Propósito
+          </h1>
+          <p className="mb-8 max-w-md text-2xl leading-7">
+            Explore nossa seleção de produtos que respeitam o meio ambiente e
+            ajudam a reduzir seu impacto ecológico. Vamos juntos construir um
+            futuro sustentável!
+          </p>
+          <button
+            className="flex w-1/2 justify-center rounded-md bg-[#B27538] py-3
+                                      font-semibold  uppercase hover:bg-[#b17d4a]"
+          >
+            Explore nossos produtos
+          </button>
+        </div>
+        <img
+          className="relative z-10"
+          src="https://s3-alpha-sig.figma.com/img/29b7/546f/0ceffa3e73a9d5404679fa684dfc3dbe?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=li-9XLUpJgZA1OGLBraBSkYMPdhAtPw67qmIWQixpbTsCSud5eOsaNfh4HGBvcCboisR0h3miJO78~oVjvikPGcW0gkvcixaVRVPbCXuobJaP6fqA0P~KNnwRK0fi6YTNfhe3nayWtkbThMWvMfOePpV3z8DFrcgFaxfRjXzAh~BaNbtcNJawq1-oJ52adDzeM8ugWyA-66-fF~qntJ0CiYF3m6jkCdNaULNUzzxIoyQcEeDx9gz0p6vn~zHb5N20eDyVIqu7PZUXCCj0LZakXcyS02m1Pl84bvvOwP5T5HL2D0HBGFFhSihMya6qNpu1A4ojMZ3zyQvlj7861EU0w__"
+          alt=""
+        />
+      </div>
+      {/* TESTE FINAL */}
+
+      {/* container2 */}
+      <div className="grid grid-cols-3">
+        <div className="col-span-1">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/6944/aabd/efee0823a7432c9f61adcd019f14e196?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nyrB3ACnZb0bPyDQF6xWnUHq5lKNL--q926DoMlSzYpJSL4GzCeyrPFaZuvrPaP~Kc3fgDCSeL3qg1w1N9E3bFY1N8yTzBM1X6y52fTGtRzekyFT32-d1AUygLUw~hHwi9LyawRzEfvGUiTR8W~vhiaeF890gN6JKn750lGSjcicIZJf~lXorPgrHVr2Du0~3KkRhjT5aD-S14Xvo4MXaEzIlTb5R-eEjjxR4pY7HTEFKyShn44oq8H1J77i78AT2IIp-5M86aW0~DkUr4B1XWKiau3UVZ8uDdhldzIjPUfWRScDDaFaSyW8duSttWqldoXmYsUEC7K8Y~qEpUCvQA__"
+            alt="Foto 1"
+            className="h-auto w-full"
+          />
+        </div>
+        <div className="mx-auto flex flex-col overflow-hidden bg-[#b27538] text-center text-white">
+          <div className="align-center font-poppins">
+            <h1 className="my-8 text-2xl font-bold  md:text-5xl">Visão</h1>
+            <p className="mx-8 text-xl">
+              Ser uma plataforma de e-commerce sustentável em promover práticas
+              de consumo e impulsionar a economia verde
+            </p>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <img
+            src="https://ik.imagekit.io/fxpct4486/ImgLinks/mulher%20(1).png?updatedAt=1717636806793"
+            alt="Foto 3"
+            className="h-auto w-full"
+          />
+        </div>
+        {/* Fotos na parte inferior */}
+        <div className="mx-auto flex flex-col overflow-hidden bg-[#b27538] text-center text-white">
+          <div className="align-center">
+            <h1 className="my-8 font-poppins text-2xl font-bold md:text-5xl">
+              Missão
+            </h1>
+            <p className="mx-8 text-xl">
+              Oferecer aos consumidores acesso a produtos ecológicos e
+              sustentáveis, educando e inspirando mudanças nos hábitos de
+              consumo que contribuam para a preservação do meio ambiente e o
+              bem-estar das futuras gerações.
+            </p>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <img
+            src="https://ik.imagekit.io/fxpct4486/ImgLinks/mao%20(1).png?updatedAt=1717638558779"
+            alt="Foto 5"
+            className="h-auto w-full"
+          />
+        </div>
+        <div className="mx-auto flex flex-col overflow-hidden bg-[#b27538] text-center text-white">
+          <div className="align-center">
+            <h1 className="my-8 font-poppins text-3xl font-bold md:text-5xl">
+              Valores
+            </h1>
+            <p className="mx-8 text-xl">
+              Sustentabilidade, Transparência, Inovação, Educação,
+              Responsabilidade Social, Qualidade, Integridade, Colaboração.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* container3 */}
+      <div className="relative text-white">
+        <img
+          className="absolute left-0 top-0 z-10 h-auto w-full"
+          src="https://s3-alpha-sig.figma.com/img/29b7/546f/0ceffa3e73a9d5404679fa684dfc3dbe?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=li-9XLUpJgZA1OGLBraBSkYMPdhAtPw67qmIWQixpbTsCSud5eOsaNfh4HGBvcCboisR0h3miJO78~oVjvikPGcW0gkvcixaVRVPbCXuobJaP6fqA0P~KNnwRK0fi6YTNfhe3nayWtkbThMWvMfOePpV3z8DFrcgFaxfRjXzAh~BaNbtcNJawq1-oJ52adDzeM8ugWyA-66-fF~qntJ0CiYF3m6jkCdNaULNUzzxIoyQcEeDx9gz0p6vn~zHb5N20eDyVIqu7PZUXCCj0LZakXcyS02m1Pl84bvvOwP5T5HL2D0HBGFFhSihMya6qNpu1A4ojMZ3zyQvlj7861EU0w__"
+          alt=""
+        />
+
+        <img
+          className="md:p-15 absolute top-0 z-20 mx-20 my-20 h-auto max-w-6xl rounded-lg"
+          src="https://s3-alpha-sig.figma.com/img/70d3/55f7/4980cb8143e79b5cefaa7af0850ab4d7?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Bx1q4ZhU6v83pQY1lrQ4hq~thmIUc-SAYKulDFjAGoSvRxaEKFZzVKhaff9PYB4YC25TfFN~17apCJzWhsvVxkkznrvZXLwF6UvqHbmFyibYO5aGGTKlphBxha0pXTvZdf2MG-Bx3HOr7EnuEM0jRQfrcIdrJbW98hTXjc8oY4so9QN9NYnXWdix5e-Ec5twAixvjNv91HloevjxdlvVf2xhxxfQohwztPUPhbw8jEHCeci5ACPG-kwAu5DyFQW0yWAAti-7d5ZT6TDFlcbIq84OkfppJkaY9rpEGxyR3XpzcGs4vmZmeOG9bcmLkLMS~OCzItKgCah0PFm2Fb84Lg__"
+          alt=""
+        />
+        <div>
+          {/* <div className="relative "> */}
+
+          {/* </div> */}
+          <div className="absolute left-0 top-20 z-30 mx-5 my-20 h-auto rounded-lg font-poppins md:p-20 ">
+            <h1 className="mx-12 mb-10 max-w-2xl whitespace-nowrap text-5xl font-bold">
+              Sobre Nós
+            </h1>
+
+            <p className="ml-10 max-w-80 text-left text-2xl">
+              Na GreenEconomy, nosso objetivo é criar um mundo onde o consumo
+              consciente seja a norma, não a exceção. Oferecemos uma seleção
+              curada de produtos sustentáveis que ajudam a reduzir o impacto
+              ambiental e promovem um futuro mais saudável para todos. Junte-se
+              a nós nessa jornada para uma economia verde e descubra como cada
+              compra pode fazer a diferença.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
