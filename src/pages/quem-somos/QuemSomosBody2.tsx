314ca8bdc33c327b8a1f8317e6af0edefcbc1501
jorgeguedess <jorge_guedes00@hotmail.com>
2024-06-07 11:44:43 -0300

    refactor: alterado o quem somos

---
+++ src/pages/quem-somos/QuemSomosBody2.tsx
@@ -0,0 +1,109 @@
+export default function QuemSomosBody2() {
+  return (
+    // principal
+    <div className="text-white">
+      {/* container1 */}
+      <div className="bg-[url('https://s3-alpha-sig.figma.com/img/29b7/546f/0ceffa3e73a9d5404679fa684dfc3dbe?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=li-9XLUpJgZA1OGLBraBSkYMPdhAtPw67qmIWQixpbTsCSud5eOsaNfh4HGBvcCboisR0h3miJO78~oVjvikPGcW0gkvcixaVRVPbCXuobJaP6fqA0P~KNnwRK0fi6YTNfhe3nayWtkbThMWvMfOePpV3z8DFrcgFaxfRjXzAh~BaNbtcNJawq1-oJ52adDzeM8ugWyA-66-fF~qntJ0CiYF3m6jkCdNaULNUzzxIoyQcEeDx9gz0p6vn~zHb5N20eDyVIqu7PZUXCCj0LZakXcyS02m1Pl84bvvOwP5T5HL2D0HBGFFhSihMya6qNpu1A4ojMZ3zyQvlj7861EU0w__')] bg-cover bg-no-repeat md:py-12">
+        {/* TESTE INICIO */}
+        <div className="container">
+          <div className="w-fit bg-primary/60 py-4 pl-10 pr-20">
+            <h1 className="my-8 font-poppins  font-semibold md:text-6xl">
+              Green Economy
+            </h1>
+            <p className="mb-8 max-w-lg text-2xl leading-7">
+              Economia verde para um futuro sustentável e de consumo
+              responsável. Uma nova forma de ver o mundo, o mercado e a economia
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* TESTE FINAL */}
+
+      {/* container2 */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="col-span-1">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/6944/aabd/efee0823a7432c9f61adcd019f14e196?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nyrB3ACnZb0bPyDQF6xWnUHq5lKNL--q926DoMlSzYpJSL4GzCeyrPFaZuvrPaP~Kc3fgDCSeL3qg1w1N9E3bFY1N8yTzBM1X6y52fTGtRzekyFT32-d1AUygLUw~hHwi9LyawRzEfvGUiTR8W~vhiaeF890gN6JKn750lGSjcicIZJf~lXorPgrHVr2Du0~3KkRhjT5aD-S14Xvo4MXaEzIlTb5R-eEjjxR4pY7HTEFKyShn44oq8H1J77i78AT2IIp-5M86aW0~DkUr4B1XWKiau3UVZ8uDdhldzIjPUfWRScDDaFaSyW8duSttWqldoXmYsUEC7K8Y~qEpUCvQA__"
+            alt="Foto 1"
+            className="h-auto w-full"
+          />
+        </div>
+        <div className="mx-auto flex flex-col overflow-hidden bg-[#b27538] text-center text-white">
+          <div className="align-center font-poppins">
+            <h1 className="text-new my-8 font-poppins text-2xl font-semibold md:text-5xl">
+              Visão
+            </h1>
+            <p className="mx-8 text-xl">
+              Ser uma plataforma de e-commerce sustentável em promover práticas
+              de consumo e impulsionar a economia verde
+            </p>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <img
+            src="https://ik.imagekit.io/fxpct4486/ImgLinks/mulher%20(1).png?updatedAt=1717636806793"
+            alt="Foto 3"
+            className="h-auto w-full"
+          />
+        </div>
+        {/* Fotos na parte inferior */}
+        <div className="mx-auto flex flex-col overflow-hidden bg-[#b27538] text-center text-white">
+          <div className="align-center">
+            <h1 className="my-8 font-poppins text-2xl font-semibold md:text-5xl">
+              Missão
+            </h1>
+            <p className="mx-8 text-xl">
+              Oferecer aos consumidores acesso a produtos ecológicos e
+              sustentáveis, educando e inspirando mudanças nos hábitos de
+              consumo que contribuam para a preservação do meio ambiente e o
+              bem-estar das futuras gerações.
+            </p>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <img
+            src="https://ik.imagekit.io/fxpct4486/ImgLinks/mao%20(1).png?updatedAt=1717638558779"
+            alt="Foto 5"
+            className="h-auto w-full"
+          />
+        </div>
+        <div className="mx-auto flex flex-col overflow-hidden bg-[#b27538] text-center text-white">
+          <div className="align-center">
+            <h1 className="my-8 font-poppins text-3xl font-semibold md:text-5xl">
+              Valores
+            </h1>
+            <p className="mx-8 text-xl">
+              Sustentabilidade, Transparência, Inovação, Educação,
+              Responsabilidade Social, Qualidade, Integridade, Colaboração.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[url('https://s3-alpha-sig.figma.com/img/29b7/546f/0ceffa3e73a9d5404679fa684dfc3dbe?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=li-9XLUpJgZA1OGLBraBSkYMPdhAtPw67qmIWQixpbTsCSud5eOsaNfh4HGBvcCboisR0h3miJO78~oVjvikPGcW0gkvcixaVRVPbCXuobJaP6fqA0P~KNnwRK0fi6YTNfhe3nayWtkbThMWvMfOePpV3z8DFrcgFaxfRjXzAh~BaNbtcNJawq1-oJ52adDzeM8ugWyA-66-fF~qntJ0CiYF3m6jkCdNaULNUzzxIoyQcEeDx9gz0p6vn~zHb5N20eDyVIqu7PZUXCCj0LZakXcyS02m1Pl84bvvOwP5T5HL2D0HBGFFhSihMya6qNpu1A4ojMZ3zyQvlj7861EU0w__')] bg-cover bg-center bg-no-repeat py-3 text-white">
+        <div className="w-full px-[80px]">
+          <div className="rounded-md bg-[url('https://s3-alpha-sig.figma.com/img/70d3/55f7/4980cb8143e79b5cefaa7af0850ab4d7?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Bx1q4ZhU6v83pQY1lrQ4hq~thmIUc-SAYKulDFjAGoSvRxaEKFZzVKhaff9PYB4YC25TfFN~17apCJzWhsvVxkkznrvZXLwF6UvqHbmFyibYO5aGGTKlphBxha0pXTvZdf2MG-Bx3HOr7EnuEM0jRQfrcIdrJbW98hTXjc8oY4so9QN9NYnXWdix5e-Ec5twAixvjNv91HloevjxdlvVf2xhxxfQohwztPUPhbw8jEHCeci5ACPG-kwAu5DyFQW0yWAAti-7d5ZT6TDFlcbIq84OkfppJkaY9rpEGxyR3XpzcGs4vmZmeOG9bcmLkLMS~OCzItKgCah0PFm2Fb84Lg__')] bg-cover bg-center bg-no-repeat">
+            <div className="ml-10 h-auto rounded-lg py-[7rem] font-poppins">
+              <div className="w-fit rounded-md bg-black/60 p-4">
+                <h1 className="mx-12 mb-10 max-w-2xl whitespace-nowrap font-poppins text-5xl font-medium">
+                  Sobre Nós
+                </h1>
+
+                <div className="ml-10 max-w-md text-left text-lg">
+                  <p>
+                    Na GreenEconomy, selecionamos rigorosamente produtos
+                    ecológicos e responsáveis, garantindo sustentabilidade. Além
+                    de vender produtos sustentáveis, educamos nossos clientes
+                    sobre práticas de consumo consciente. Suas compras promovem
+                    uma economia verde, reduzindo o impacto ambiental e
+                    incentivando o uso de recursos renováveis.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
