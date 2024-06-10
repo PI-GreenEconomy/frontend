ae39f7da49b383decc0180b66b22d329e85c1df5
jorgeguedess <jorge_guedes00@hotmail.com>
2024-06-10 14:59:21 -0300

    chore: configurado variaveis de ambiente .env

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-7ggr.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const cadastrarUsuario = async (
