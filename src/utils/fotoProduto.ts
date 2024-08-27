5fdd67f7d0f8f499c0ea4eb1324ea2fe31866846
jorgeguedess <jorge_guedes00@hotmail.com>
2024-08-27 09:37:01 -0300

    fix: ajustado nova url da imagem de produto padrão

--- src/utils/fotoProduto.ts
+++ src/utils/fotoProduto.ts
@@ -1,5 +1,5 @@
 const defaultPhotoUrl =
-  "https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png";
+  "https://ik.imagekit.io/GreenEconomy/default-product-image.png?updatedAt=1724762147450";
 
 export function transformarFotoProduto(foto: string) {
   return foto?.length > 30 ? foto : defaultPhotoUrl;
